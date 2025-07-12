af960776083bd0055bf95a0a6b7e9ef873072301
matricarin <matricarin@gmail.com>
2025-07-12 15:05:59 +0500

    1084

---
+++ timus/in_progress/timus_1084.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace timus.in_progress
+{
+    public class timus_1084
+    {
+        public static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+
+            var a = input[0];
+            var r = input[1];
+
+            
+        }
+    }
+}
