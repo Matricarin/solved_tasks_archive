28a74ff41dd036cc3aa783d3c7021afba81adbb3
matricarin <matricarin@gmail.com>
2026-06-19 12:39:10 +0500

    updated

--- yandex/Graphs/Componets/TaskA.cs
+++ yandex/Graphs/Componets/TaskA.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.VisualBasic;
 
 namespace yandex.Graphs.Componets
 {
@@ -13,19 +12,23 @@ namespace yandex.Graphs.Componets
             System.Globalization.CultureInfo.DefaultThreadCurrentCulture =
                                 System.Globalization.CultureInfo.InvariantCulture;
            
-            var input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
 
-            var adjList = Enumerable.Range(1, input[0]).ToDictionary(k => k, v => new HasSet<int>());
+            var adjList = Enumerable.Range(1, input[0]).ToDictionary(k => k, v => new HashSet<int>());
 
             for(int i = 0; i < input[1]; i++)
             {
-                var vertices = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                var vertices = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
 
                 adjList[vertices[0]].Add(vertices[1]);
                 adjList[vertices[1]].Add(vertices[0]);
             }
 
-            //  найти компоненты связности?
+            
         }
     }
 }
