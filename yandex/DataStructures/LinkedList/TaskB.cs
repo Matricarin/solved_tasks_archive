8f96e587761a8e1061346269ae67d5480cf14627
matricarin <matricarin@gmail.com>
2026-06-19 18:35:11 +0500

    task b solved

---
+++ yandex/DataStructures/LinkedList/TaskB.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace yandex.LinkedList
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            var array = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var solver = new PriceSolver();
+
+            var a = solver.Solve(array);
+
+            Console.WriteLine($"{a[0]} {a[1]}");
+
+            Console.WriteLine($"{a[2]} {a[3]}");
+        }
+    }
+
+    public sealed class PriceSolver
+    {
+        public int[] Solve(int[] array)
+        {
+            var bestTrackedMaxIdx = 0;
+            var bestTrackedMinIdx = 0;
+
+            var resMaxI = 0;
+            var resMaxJ = 1;
+
+            var resMinI = 0;
+            var resMinJ = 1;
+
+            var max = array[resMaxI] - array[resMaxJ];
+            var min = array[resMinI] - array[resMinJ];
+
+            for (var j = 1; j < array.Length; j++)
+            {
+                var maxDiff = array[bestTrackedMaxIdx] - array[j];
+
+                if (maxDiff > max)
+                {
+                    max = maxDiff;
+                    resMaxI = bestTrackedMaxIdx;
+                    resMaxJ = j;
+                }
+
+                if (array[bestTrackedMaxIdx] < array[j])
+                {
+                    bestTrackedMaxIdx = j;
+                }
+
+                var minDiff = array[bestTrackedMinIdx] - array[j];
+
+                if (minDiff < min)
+                {
+                    min = minDiff;
+                    resMinI = bestTrackedMinIdx;
+                    resMinJ = j;
+                }
+
+                if (array[bestTrackedMinIdx] > array[j])
+                {
+                    bestTrackedMinIdx = j;
+                }
+            }
+
+            return [ resMinI + 1, resMinJ + 1, resMaxI + 1, resMaxJ + 1,];
+        }
+    }
+}
