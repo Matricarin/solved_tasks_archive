d53276c294de95318ed5158e827dbb26a12b23a1
matricarin <matricarin@gmail.com>
2026-06-21 13:27:19 +0500

    updated

--- yandex/Graphs/Componets/TaskA.cs
+++ yandex/Graphs/Componets/TaskA.cs
@@ -28,7 +28,47 @@ namespace yandex.Graphs.Componets
                 adjList[vertices[1]].Add(vertices[0]);
             }
 
-            
+            var used = new bool[input[0]];
+
+            var components = new List<List<int>>();
+
+            foreach(var vertice in adjList)
+            {
+                if(used[vertice.Key - 1])
+                {
+                    continue;
+                }
+                var component = new List<int>();
+
+                DFS(vertice.Key, adjList, used, ref component);
+
+                components.Add(component);
+            }
+
+            if(components.Count > 1)
+            {
+                Console.WriteLine("NO");
+            }
+            else
+            {
+                Console.WriteLine("YES");
+            }
+        }
+
+        private static void DFS(int vertice, Dictionary<int, HashSet<int>> adjList, bool[] used, ref List<int> component)
+        {
+            used[vertice - 1]= true;
+
+            var vertices = adjList[vertice];
+
+            foreach(var v in vertices)
+            {
+                if(!used[v -1])
+                {
+                    component.Add(v);
+                    DFS(v, adjList, used, ref component);
+                }
+            }
         }
     }
 }
