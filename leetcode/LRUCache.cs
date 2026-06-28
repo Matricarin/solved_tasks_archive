0ddad2039f1bb8f3fd93bf7b39d4905aadcfa4b9
matricarin <matricarin@gmail.com>
2026-06-28 12:21:52 +0500

    updated

--- leetcode/LRUCache.cs
+++ leetcode/LRUCache.cs
@@ -1,22 +1,22 @@
 public class LRUCache
 {
-    private LinkedList<int> _list;
-    private Dictionary<int,int> _dict;
+    private LinkedList<ValueTuple<int, int>> _list;
+    private Dictionary<int, LinkedListNode<ValueTuple<int, int>>> _dict;
     private readonly int _capacity;
     public LRUCache(int capacity)
     {
         _capacity = capacity;
-        _dict = new Dictionary<int,int>(capacity);
-        _list = new LinkedList<int>();
+        _dict = new Dictionary<int, LinkedListNode<ValueTuple<int, int>>>(capacity);
+        _list = new LinkedList<ValueTuple<int, int>>();
     }
 
     public int Get(int key)
     {
         if(_dict.TryGetValue(key, out var value))
         {
-            _list.Remove(key);
-            _list.AddFirst(key);
-            return value;
+            _list.Remove(value);
+            _list.AddFirst(value);
+            return value.Value.Item2;
         }
         else
         {
@@ -28,24 +28,25 @@ public class LRUCache
     {
         if(_dict.ContainsKey(key))
         {
-            _dict[key] = value;
-            _list.Remove(key);
-            _list.AddFirst(key);
+            _dict[key].Value = new (key, value);
+            _list.Remove(_dict[key]);
+            _list.AddFirst(_dict[key]);
         }
         else
         {
+            var item = new LinkedListNode<ValueTuple<int, int>>(new (key, value));
             if(_dict.Count < _capacity)
             {
-                _dict.Add(key, value);
-                _list.AddFirst(key);
+                _dict.Add(key, item);
+                _list.AddFirst(item);
             }
             else
             {
                 var last = _list.Last;
                 _list.RemoveLast();
-                _list.AddFirst(key);
-                _dict.Remove(last.Value);
-                _dict.Add(key, value);
+                _list.AddFirst(item);
+                _dict.Remove(last.Value.Item1);
+                _dict.Add(key, item);
             }
         }
         
