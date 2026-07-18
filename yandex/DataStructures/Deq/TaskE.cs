50f38a993646a3cb040dfc02bef8c266c11b68b9
matricarin <matricarin@gmail.com>
2026-07-18 08:42:09 +0500

    attempt

--- yandex/DataStructures/Deq/TaskE.cs
+++ yandex/DataStructures/Deq/TaskE.cs
@@ -23,7 +23,7 @@ namespace yandex.DataStructures.Deq
 
             var deq = new LinkedList<int>(array);
 
-            while(deq.Count > 3)
+            while(deq.Count > 2)
             {
                 var current = deq.First;
                 var left = deq.Last;
@@ -52,60 +52,36 @@ namespace yandex.DataStructures.Deq
                     }
                 }
 
-                if(current.Value > left.Value)
+                if(deq.Count != 2)
                 {
-                    if(current.Value > right.Value)
+                    if(current.Value > left.Value)
                     {
-                        deq.Remove(current);
-                        deq.AddFirst(current);
+                        if(current.Value > right.Value)
+                        {
+                            deq.Remove(current);
+                            deq.AddFirst(current);
+                        }
+                        else
+                        {
+                            deq.Remove(right);
+                            deq.AddFirst(right);
+                        }
                     }
                     else
                     {
-                        deq.Remove(right);
-                        deq.AddFirst(right);
+                        if(left.Value > right.Value)
+                        {
+                            deq.Remove(left);
+                            deq.AddFirst(left);
+                        }
+                        else
+                        {   
+                            deq.Remove(right);
+                            deq.AddFirst(right);
+                        }
                     }
-                }
-                else
-                {
-                    if(left.Value > right.Value)
-                    {
-                        deq.Remove(left);
-                        deq.AddFirst(left);
-                    }
-                    else
-                    {   
-                        deq.Remove(right);
-                        deq.AddFirst(right);
-                    }
-                }
-            }
-
-            var c = deq.First;
-            var l = deq.Last;
-            var r = c.Next;
-
-            if(c.Value < l.Value)
-            {
-                if(c.Value < r.Value)
-                {
-                    deq.Remove(c);
-                }
-                else
-                {
-                    deq.Remove(r);
-                }
-            }
-            else
-            {
-                if(l.Value < r.Value)
-                {
-                    deq.Remove(l);
-                }
-                else
-                {
-                    deq.Remove(r);
-                }
-            }
+                }                
+            }            
 
             writer.WriteLine(string.Join(" ", deq));
         }
