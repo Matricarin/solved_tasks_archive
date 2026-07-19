f3660068a3910c645b8cae9dd0e8692c0235ff9b
matricarin <matricarin@gmail.com>
2026-07-19 09:57:49 +0500

    O(2n)

---
+++ yandex/Basic/MaximumProduct/TaskA.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace yandex.Basic.MaximumProduct
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using var reader = new StreamReader("input.txt");
+            using var writer = new StreamWriter("output.txt");
+
+            var n = long.Parse(reader.ReadLine());
+
+            var array = Array.ConvertAll
+            (
+                reader.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries),
+                long.Parse
+            );
+
+            long max = long.MinValue;
+            int maxIndex = int.MinValue;
+            long previousMax = long.MinValue;
+
+            for(int i = 0; i < n; i++)
+            {
+                if(max < array[i])
+                {
+                    max = array[i];
+                    maxIndex = i;
+                }
+            }
+
+            for(int j = 0; j < n; n++)
+            {
+                if(maxIndex == j)
+                {
+                    continue;
+                }
+
+                if(previousMax < array[j])
+                {
+                    previousMax = array[j];
+                }
+            }
+
+            long maxProduct = max * previousMax;
+
+            writer.WriteLine(max);
+        }
+    }
+}
