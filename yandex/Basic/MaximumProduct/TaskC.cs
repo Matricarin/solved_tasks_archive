2a621063960871dec206abbf304b3fffecedfb25
matricarin <matricarin@gmail.com>
2026-07-19 22:18:32 +0500

    more beauty solution

--- yandex/Basic/MaximumProduct/TaskC.cs
+++ yandex/Basic/MaximumProduct/TaskC.cs
@@ -1,7 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.IO;
 
 namespace yandex.Basic.MaximumProduct
@@ -14,55 +11,20 @@ namespace yandex.Basic.MaximumProduct
             using var writer = new StreamWriter("output.txt");
 
             var n = int.Parse(reader.ReadLine());
-
-            var arr = Array.ConvertAll
-            (
+            var arr = Array.ConvertAll(
                 reader.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries),
                 long.Parse
             );
 
-            if (n == 3)
-            {
-                writer.WriteLine(arr[0] * arr[1] * arr[2]);
-                return;
-            }
-
             Array.Sort(arr);
-            long max = long.MinValue;
-            long r2 = arr[^1] * arr[^2];
-            long l2 = arr[0] * arr[1];
 
-            if (r2 > l2)
-            {
-                if (arr[0] < 0)
-                {
-                    if (l2 * arr[^1] > r2 * arr[^3])
-                    {
-                        max = l2 * arr[^1];
-                    }
-                    else
-                    {
-                        max = r2 * arr[^3];
-                    }
-                }
-                else
-                {
-                    max = r2 * arr[^3];
-                }
-            }
-            else
-            {
-                if (arr[^1] > 0)
-                {
-                    max = l2 * arr[^1];
-                }
-                else
-                {
-                    max = r2 * arr[^3];
-                }
-            }
+            long option1 = arr[^1] * arr[^2] * arr[^3];
+            
+            long option2 = arr[0] * arr[1] * arr[^1];
 
-            writer.WriteLine(max);
+            long maxProduct = Math.Max(option1, option2);
+
+            writer.WriteLine(maxProduct);
         }
     }
 }
