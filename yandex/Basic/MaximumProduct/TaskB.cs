f2669df4f246bb1aec4af3a97418b302673e3a40
matricarin <matricarin@gmail.com>
2026-07-19 20:42:32 +0500

    first try

---
+++ yandex/Basic/MaximumProduct/TaskB.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace yandex.Basic.MaximumProduct
+{
+    public class TaskB
+    {
+        public static void Main(string[] args)
+        {
+            using var reader = new StreamReader("input.txt");
+            using var writer = new StreamWriter("output.txt");
+
+            var n = int.Parse(reader.ReadLine());
+
+            if(n <= 2)
+            {
+                writer.WriteLine("No");
+            }
+            else
+            {
+                double c = n + (n - 3) * 2.0;
+                double d = c / n;
+                if(d <= 1.5)
+                {
+                    writer.WriteLine("No");
+                }
+                else
+                {
+                    writer.WriteLine("Yes");
+                    writer.WriteLine(string.Join(" ", Enumerable.Range(1, n).OrderByDescending(i => i)));
+                }
+            }
+        }
+    }
+}
