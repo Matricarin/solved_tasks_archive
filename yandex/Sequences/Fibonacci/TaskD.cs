652df40608eeb2d6c2250757c2001049c6b5b897
matricarin <matricarin@gmail.com>
2026-07-22 16:47:54 +0500

    solution with pisano

--- yandex/Sequences/Fibonacci/TaskD.cs
+++ yandex/Sequences/Fibonacci/TaskD.cs
@@ -6,7 +6,7 @@ using System.IO;
 
 namespace yandex.Sequences.Fibonacci
 {
-    public class TaskD
+    public static class TaskD
     {
         public static void Main(string[] args)
         {
@@ -14,32 +14,61 @@ namespace yandex.Sequences.Fibonacci
             using var writer = new StreamWriter("output.txt");
 
             var n = long.Parse(reader.ReadLine());
-
-            var old = 0;
-            var next = 1;
-
-            if(n == 0)
-            {
-                writer.WriteLine(0);
-                return;
-            }
             
-            if(n == 1)
-            {
-                writer.WriteLine(1);
-                return;
-            }
-            var result = 1;
+            var period = GetPisano();
             
-            for(long i = 2; i <= n; i++)
-            {
-                var newOld = next;
-                next = (old + next) % 10;
-                result += next;
-                old = newOld;
-            }
+            var sumPeriod = GetSumModFibonacci(period);
+
+            var x = n / period;
+            var y = n % period;
+
+            var result = sumPeriod * x + GetSumModFibonacci(y);
 
             writer.WriteLine(result % 10);
+        }
+
+        public static long GetSumModFibonacci(long n)
+        {
+            long result = 1;
+            
+            if(n == 0)
+            {
+                return 0;
+            }
+
+            if(n == 1)
+            {
+                return 1;
+            }
+            long old = 0;
+            long next = 1;
+            for(long i = 2; i <= n; i++)
+            {
+                var oldNew = next;
+                next = (old + next) % 10;
+                old = oldNew;
+                result += next;
+            }
+
+            return result;
+        }
+
+        public static long GetPisano()
+        {
+            long old = 0;
+            long next = 1;
+            long period = 0;
+            while(true)
+            {
+                long oldNew = next;
+                next = (old + next) % 10;
+                old = oldNew;
+                period++;
+                if(old == 0 & next == 1)
+                {
+                    return period;
+                }
+            }
         }
     }
 }
