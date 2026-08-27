4981955355412077d7dd0100ac2d3dcc67ade110
matricarin <matricarin@gmail.com>
2026-08-27 06:06:22 +0500

    task b first try

--- yandex/Graphs/Traversal/TaskB-ns.cs
+++ yandex/Graphs/Traversal/TaskB-ns.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.IO;
-using System.Formats.Asn1;
 
 namespace yandex.Graphs.Traversal
 {
@@ -22,88 +21,84 @@ namespace yandex.Graphs.Traversal
             var x = arr[0];
             var y = arr[1];
 
-            var (m, a, d) = Evaluate(x);
+            var visited = new HashSet<int>();
 
-            var root = new Node(x);
-            root.M = m;
-            root.A = a;
-            root.D = d;
+            var adjList = new List<int>[10];
 
-            var counter = 0;
-            var q = new Queue<Node>();
-            q.Enqueue(root);
-
-            while (q.Count > 0)
+            for(int i = 0; i < 10; i++)
             {
-                var levelSize = q.Count;
-
-                Node found = null;
-
-                for (int i = 0; i < levelSize; i++)
+                adjList[i] = new List<int>();
+                var a = x + i;
+                var m = x + i;
+                var d = x - i;
+                if(visited.Add(a))
                 {
-                    var current = q.Dequeue();
+                    adjList[i].Add(a);
+                }
+                if(visited.Add(d))
+                {
+                    adjList[i].Add(d);
+                }                
+                if(visited.Add(m))
+                {
+                    adjList[i].Add(m);
+                }
+            }
 
-                    if (current.M == null && current.A == null && current.D == null)
+            var depth = 0;
+            var notFound = true;
+            while(notFound)
+            {
+                depth++;
+                var q = new Queue<int>();
+                for(int k = 0; k < 10; k++)
+                {
+                    if(!notFound)
                     {
-                        var (mNodes, aNodes, dNodes) = Evaluate(current.Value);
-                        current.M = mNodes;
-                        current.D = dNodes;
-                        current.A = aNodes;
-                    }
-
-                    for (int k = 0; k < 10; k++)
-                    {
-                        q.Enqueue(current.M[k]);
-                        q.Enqueue(current.A[k]);
-                        q.Enqueue(current.D[k]);
-                    }
-
-                    if (current.Value == y)
-                    {
-                        found = current;
                         break;
                     }
-                }
+                    var list = adjList[k];
+                    foreach(var item in list)
+                    {
+                        q.Enqueue(item);
+                    }
+                    
+                    adjList[k] = new List<int>();
 
-                if (found != null)
-                {
-                    break;
-                }
+                    while(q.Count > 0)
+                    {
+                        var c = q.Dequeue();
 
-                counter++;
+                        var na = c + k;
+                        var nd = c - k;
+                        var nm = c * k;
+
+                        if(na == y || nd == y || nm == y)
+                        {
+                            notFound = false;
+                        }
+
+                        if(visited.Add(na))
+                        {
+                            adjList[k].Add(na);
+                        }
+                        if(visited.Add(nd))
+                        {
+                            adjList[k].Add(nd);
+                        }                
+                        if(visited.Add(nm))
+                        {
+                            adjList[k].Add(nm);
+                        }
+                    }
+                }
             }
 
-            writer.WriteLine(counter);
+            writer.WriteLine(depth);
         }
 
-        private static (Node[], Node[], Node[]) Evaluate(int val)
-        {
-            var m = new Node[10];
-            var a = new Node[10];
-            var d = new Node[10];
-            for (int i = 0; i < 10; i++)
-            {
-                m[i] = new Node(val * i);
-                a[i] = new Node(val + i);
-                d[i] = new Node(val - i);
-            }
-
-            return (m, a, d);
-        }
     }
-
-    public class Node
-    {
-        public int Value;
-        public Node[] M;
-        public Node[] A;
-        public Node[] D;
-
-        public Node(int val)
-        {
-            Value = val;
-        }
-    }
+   
 }
 
 
